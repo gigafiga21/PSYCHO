9ab63d07154acf2d6a8f9a7e99c4a5868f6ac404
gigafiga21 <gigafiga21@gmail.com>
2019-04-28 14:32:23 +0300

    Added translation for the Score class (issue #3).

--- BIN/bin.js
+++ BIN/bin.js
@@ -1191,6 +1191,25 @@ function Breaker(parent)
 function Score(parent, score)
 {
     /**
+     * Translates all strings in the class to given language
+     * @param {String} language - language to change strings
+     */
+    function translate(language)
+    {
+        DOM.header.innerHTML = strings[translations.current].score;
+    }
+
+    /**
+     * Translated strings
+     * @type {Array}
+     */
+    var strings =
+        {
+            "EN": {score: "Score:"},
+            "RU": {score: "Результат:"}
+        };
+
+    /**
      * DOM tree of the class
      * @type {Object}
      */
@@ -1202,8 +1221,9 @@ function Score(parent, score)
         };
     
     DOM.container = parent.newChildElement("div", {classList: "score__container"});
-    DOM.header = DOM.container.newChildElement("span", {classList: "score__header"}, "Результат:");
+    DOM.header = DOM.container.newChildElement("span", {classList: "score__header"}, strings[translations.current].score);
     DOM.score = DOM.container.newChildElement("span", {classList: "score__result"}, "" + score);
+    translations.addEventListener(translate.bind(this));
 }
 
 /**
